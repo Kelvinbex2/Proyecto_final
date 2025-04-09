6c38b7a325a5388def47b06800e5c011ab2a0b68
Noma kelvin <obamedo20@gmail.com>
2025-04-09 19:42:15 +0200

    Graphics implemented 1.1

---
+++ proyexto_final/scene/ui/script_ui/window_mode_button.gd
@@ -0,0 +1,36 @@
+extends Control
+
+const WINDOW_MODE_ARRAY : Array[String] = [
+	"Full-Screen",
+	"Window Mode",
+	"Borderless Window",
+	"Borderless Full-Screen"
+]
+
+@onready var option_button: OptionButton = $HBoxContainer/OptionButton
+
+
+func _ready() -> void:
+	add_window_mode_item()
+	option_button.item_selected.connect(on_window_mode_selected)
+
+
+func add_window_mode_item()-> void:
+	for mode in WINDOW_MODE_ARRAY:
+		option_button.add_item(mode)
+		
+		
+func  on_window_mode_selected(index: int) -> void:
+	match index:
+		0:
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+			DisplayServer.window_set_flag(DisplayServer.WINDOW_FLAG_BORDERLESS,false)
+		1:
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+			DisplayServer.window_set_flag(DisplayServer.WINDOW_FLAG_BORDERLESS,false)
+		2:
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+			DisplayServer.window_set_flag(DisplayServer.WINDOW_FLAG_BORDERLESS,true)
+		3:
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+			DisplayServer.window_set_flag(DisplayServer.WINDOW_FLAG_BORDERLESS,true)
