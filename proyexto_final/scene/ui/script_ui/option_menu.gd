b3512eb8a474b5aaa5f4e4c40613e3de4a791407
Noma kelvin <obamedo20@gmail.com>
2025-04-11 11:06:20 +0200

    update

--- proyexto_final/scene/ui/script_ui/option_menu.gd
+++ proyexto_final/scene/ui/script_ui/option_menu.gd
@@ -1,11 +1,13 @@
 class_name OptionMenu
 extends Control
 @onready var btn_exit: Button = $MarginContainer/VBoxContainer/btnExit
+@onready var setting_con: Control = $MarginContainer/VBoxContainer/setting_con
 
 signal exit_options_menu
 
 func _ready() -> void:
 	btn_exit.button_down.connect(on_exit_pressed)
+	setting_con.exit_opt_menu.connect(on_exit_pressed) #Escape btn to exit
 	set_process(false)
 	
 	
