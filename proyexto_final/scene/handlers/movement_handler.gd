62cfe334da1f844a6d80e513930261a1065d7551
Noma kelvin <obamedo20@gmail.com>
2025-04-19 14:50:27 +0200

    fin 19/04

--- proyexto_final/scene/handlers/movement_handler.gd
+++ proyexto_final/scene/handlers/movement_handler.gd
@@ -1,17 +1,19 @@
 class_name MovementHandler
 extends Node2D
 
-@export var move_speed : int = 0
+@export var move_speed: float  = 0.0
+@export var acceleration : float =0.0
+@export var decceleration : float =0.0
 
 func handle_movement(body : CharacterBody2D, input_direc : Vector2, delta :float) -> void:
-	handle_decelartion(body,delta)
+
 	if input_direc.x < 0 :
-		body.velocity.x = -move_speed * delta
-		
+		body.velocity.x = lerp(body.velocity.x, -move_speed,acceleration*delta)
+		return
 	if input_direc.x > 0:
-		body.velocity.x = +move_speed * delta
-
-
+		body.velocity.x = lerp(body.velocity.x, +move_speed,acceleration*delta)
+		return
+	handle_decelartion(body,delta)
 
 func handle_decelartion(body: CharacterBody2D, delta : float) -> void:
-	body.velocity.x =0
+	body.velocity.x =lerp(body.velocity.x,0.0,decceleration *delta)
