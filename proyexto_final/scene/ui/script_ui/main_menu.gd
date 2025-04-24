2f38d568e001fe361bd63f536a919346f0970d97
Noma kelvin <165056190+Kelvinbex2@users.noreply.github.com>
2025-04-24 08:17:18 +0200

    updated

--- proyexto_final/scene/ui/script_ui/main_menu.gd
+++ proyexto_final/scene/ui/script_ui/main_menu.gd
@@ -3,7 +3,7 @@ extends Control
 @onready var btn_start: Button = $MarginContainer/hboxButton/VBoxContainer/btnStart
 @onready var btn_exit: Button = $MarginContainer/hboxButton/VBoxContainer/btnExit
 @onready var btn_opcion: Button = $MarginContainer/hboxButton/VBoxContainer/btnOpcion
-#@onready var start_game : PackedScene = preload()
+@onready var start_game : PackedScene = preload("res://scene/main/main.tscn")
 @onready var option_menus: OptionMenu = $option_menu
 @onready var margin_container: MarginContainer = $MarginContainer
 
@@ -12,6 +12,8 @@ func _ready() -> void:
 	handle_con_signal()
 	
 
+func on_start_pressed() -> void:
+	get_tree().change_scene_to_packed(start_game)
 
 func  on_exit_pressed() -> void:
 	get_tree().quit()
@@ -25,10 +27,12 @@ func on_exit_options_menu() -> void:
 	margin_container.visible = true
 	option_menus.visible = false
 
-func  handle_con_signal()-> void:
+func handle_con_signal() -> void:
 	btn_exit.button_down.connect(on_exit_pressed)
 	btn_opcion.button_down.connect(on_option_pressed)
+	btn_start.button_down.connect(on_start_pressed)
 	option_menus.exit_options_menu.connect(on_exit_options_menu)
+
 
 
 	
