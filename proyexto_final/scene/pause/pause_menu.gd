e7fa413bd98e5970d429fd8b6f762f3d208f5c14
Noma kelvin <165056190+Kelvinbex2@users.noreply.github.com>
2025-04-29 12:03:24 +0200

    implemented pause

--- proyexto_final/scene/pause/pause_menu.gd
+++ proyexto_final/scene/pause/pause_menu.gd
@@ -26,4 +26,4 @@ func _on_restart_pressed() -> void:
 	get_tree().reload_current_scene()
 
 func _on_exit_pressed() -> void:
-	get_tree().quit()
+	get_tree().change_scene_to_file("res://scene/ui/main_menu.tscn")
