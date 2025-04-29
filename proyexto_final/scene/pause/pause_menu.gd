27b122bcaef24f6e426a0d83864464021a1e7d01
Noma kelvin <165056190+Kelvinbex2@users.noreply.github.com>
2025-04-29 11:56:29 +0200

    restart implemented

---
+++ proyexto_final/scene/pause/pause_menu.gd
@@ -0,0 +1,29 @@
+extends Control
+
+func _ready() -> void:
+	visible = false 
+	
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("escape"):
+		if get_tree().paused:
+			resume()
+		else:
+			pause()
+
+func pause() -> void:
+	get_tree().paused = true
+	visible = true  
+
+func resume() -> void:
+	get_tree().paused = false
+	visible = false  
+
+func _on_btn_resum_pressed() -> void:
+	resume()
+
+func _on_restart_pressed() -> void:
+	get_tree().paused = false  
+	get_tree().reload_current_scene()
+
+func _on_exit_pressed() -> void:
+	get_tree().quit()
