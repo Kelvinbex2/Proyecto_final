da67a3047b239ba0edf297a81969d99c9cac5490
Noma kelvin <obamedo20@gmail.com>
2025-05-04 17:19:46 +0200

    fin

--- proyexto_final/scene/handlers/dead_zone.gd
+++ proyexto_final/scene/handlers/dead_zone.gd
@@ -6,10 +6,6 @@ func _ready() -> void:
 	body_entered.connect(on_body_entered)
 
 
-
-
-
-
 func  on_body_entered(body:Node2D) -> void:
 	if body is Player:
 		SignalBus.emit_on_player_entered_dead_zone()
