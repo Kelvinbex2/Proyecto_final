1475da5a9c5328feccbed77a393826166cb5db34
Noma kelvin <165056190+Kelvinbex2@users.noreply.github.com>
2025-05-06 12:39:12 +0200

    update

--- proyexto_final/scene/handlers/dead_zone.gd
+++ proyexto_final/scene/handlers/dead_zone.gd
@@ -6,7 +6,8 @@ func _ready() -> void:
 	body_entered.connect(on_body_entered)
 
 
-func  on_body_entered(body:Node2D) -> void:
+func on_body_entered(body:Node2D) -> void:
 	if body is Player:
 		SignalBus.emit_on_player_entered_dead_zone()
-	body.queue_free()
+		body.call_deferred("reload_scene")
+		call_deferred("queue_free") 
