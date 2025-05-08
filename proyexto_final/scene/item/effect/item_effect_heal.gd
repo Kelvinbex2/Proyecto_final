e7181d708b7d757247eb49d6484c914db9a715f8
Noma kelvin <165056190+Kelvinbex2@users.noreply.github.com>
2025-05-08 13:08:09 +0200

    update itemEfect

--- proyexto_final/scene/item/effect/item_effect_heal.gd
+++ proyexto_final/scene/item/effect/item_effect_heal.gd
@@ -5,9 +5,13 @@ extends ItemEffect
 @export var heal_amount : int = 1
 @export var sound: AudioStream
 var health_handler: HealthHandler = null
+var pause_menu: PauseMenu = null
+
 
 func use() -> void:
 	if health_handler:
 		health_handler.handle_healing(heal_amount)
-	
+		
+	if pause_menu and sound:
+		pause_menu.play_audio(sound)
 	
