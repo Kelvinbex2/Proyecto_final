841f7a33f34650213dd15987898596f51b3e9e69
Noma kelvin <165056190+Kelvinbex2@users.noreply.github.com>
2025-05-08 12:11:41 +0200

    update

--- proyexto_final/scene/pause/inventory/invetory.gd
+++ proyexto_final/scene/pause/inventory/invetory.gd
@@ -3,5 +3,19 @@ extends Resource
 
 @export var slots : Array[SlotData]
 
-func add_item () -> void:
-	pass
+func add_item (item: ItemData, count : int = 1) -> bool:
+	for j in slots:
+		if j:
+			if j.item_data == item:
+				j.quantity +=count
+				return true
+	
+	for i in slots.size():
+		if slots[i] ==null:
+			var new = SlotData.new()
+			new.item_data = item
+			new.quantity = count
+			slots[i] = new
+			return true
+		
+	return false
