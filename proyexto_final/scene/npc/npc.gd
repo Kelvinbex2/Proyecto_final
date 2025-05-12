e0dd4abc8a4eba4b6cd429b3f64a1f5ea6b2dee5
Noma kelvin <165056190+Kelvinbex2@users.noreply.github.com>
2025-05-12 12:19:09 +0200

    update npc

--- proyexto_final/scene/npc/npc.gd
+++ proyexto_final/scene/npc/npc.gd
@@ -1,3 +1,22 @@
 @icon("res://Assets/character/npc/icon/npc.svg")
 class_name Npc 
 extends CharacterBody2D
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+
+signal behaviour_enabled
+
+var state : String = "idle"
+var direction : Vector2 = Vector2.DOWN
+var direction_name : String = "down"
+
+@export var npc_resoure : NpcResource
+
+
+
+func _ready() -> void:
+	setup_npc()
+	
+	
+func setup_npc() -> void:
+	if npc_resoure:
+		animated_sprite.frames = npc_resoure.sprite_frames
--- proyexto_final/scene/npc/npc_resource.gd
+++ proyexto_final/scene/npc/npc_resource.gd
@@ -1,6 +1,7 @@
 class_name NpcResource
 extends Resource
-@export var npc_name : String =""
-@export var sprite : Texture
+
+@export var npc_name : String = ""
+@export var sprite_frames : SpriteFrames
 @export var potrait : Texture
-#@export var dialogo
+@export var dialogo_audio : String
