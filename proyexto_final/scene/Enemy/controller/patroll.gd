cd0ec145baa6346bad49c69794e34b3cb64ebe8d
Noma kelvin <165056190+Kelvinbex2@users.noreply.github.com>
2025-05-12 08:45:51 +0200

    update enemy animation

--- proyexto_final/scene/Enemy/controller/patroll.gd
+++ proyexto_final/scene/Enemy/controller/patroll.gd
@@ -7,8 +7,17 @@ extends CharacterBody2D
 @onready var gravity_handler: GravityHandler = $GravityHandler
 @onready var hurt_box: HurtBox = $HurtBox
 @onready var drop_handler: DropHandler = $Drop_handler
+@onready var detection_area: Area2D = $DetectionArea
+var can_attack := true
+var is_attacking := false
+var animation_task = null
+var has_already_attacked := false
+
+
 
 func _ready() -> void:
+	detection_area.body_entered.connect(_on_detection_area_entered)
+	detection_area.body_exited.connect(_on_detection_area_exited)
 	hurt_box.area_entered.connect(on_player_hit)
 	
 func _physics_process(delta: float) -> void:
@@ -22,3 +31,31 @@ func _physics_process(delta: float) -> void:
 
 func on_player_hit(area: Area2D)-> void:
 	drop_handler.add_coin(1)
+
+func _on_detection_area_entered(body: Node) -> void:
+	if body.is_in_group("Player") and not has_already_attacked:
+		print("Jugador detectado")
+		has_already_attacked = true  # Marcamos que ya atacó
+		play_hit_animation()
+
+
+
+func _on_detection_area_exited(body: Node) -> void:
+	if body.is_in_group("Player"):
+		print("Jugador se fue")
+		has_already_attacked = false  # Ahora puede volver a atacar la próxima vez
+
+
+func play_hit_animation() -> void:
+	if not can_attack:
+		return
+
+	can_attack = false
+	print("Reproduciendo animación 'hit'")
+	animatedSprite2D.play("hit")
+
+	await animatedSprite2D.animation_finished
+	animatedSprite2D.play("run")
+
+	await get_tree().create_timer(0.5).timeout
+	can_attack = true
--- proyexto_final/scene/handlers/enemy/ai_handler.gd
+++ proyexto_final/scene/handlers/enemy/ai_handler.gd
@@ -8,9 +8,12 @@ extends Node2D
 @export var movement : MovementHandler = null
 
 
-enum {MOVE_LEFT,MOVE_RIGHT}
+enum { MOVE_LEFT, MOVE_RIGHT, ATTACK }
+
 
 var current_state = MOVE_LEFT
+var player_in_range: bool = false
+
 
 func handle_state(body:CharacterBody2D, delta:float) -> void:
 	check_state()
@@ -19,10 +22,16 @@ func handle_state(body:CharacterBody2D, delta:float) -> void:
 			movement.handle_movement(body,Vector2.LEFT,delta)
 		MOVE_RIGHT:
 			movement.handle_movement(body,Vector2.RIGHT,delta)
+		ATTACK:
+			if body.has_method("play_hit_animation"):
+				body.play_hit_animation()
 
 
 func check_state() -> void:
-	if left_cast.is_colliding() == false or left_wall_cast.is_colliding():
+	if player_in_range:
+		current_state = ATTACK
+		return
+	if not left_cast.is_colliding() or left_wall_cast.is_colliding():
 		current_state = MOVE_RIGHT
-	if  right_cast.is_colliding() == false or right_wall_cast.is_colliding():
+	elif not right_cast.is_colliding() or right_wall_cast.is_colliding():
 		current_state = MOVE_LEFT
