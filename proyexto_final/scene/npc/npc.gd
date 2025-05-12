fea14671409d9a25e90e0fef57de3720ad00c541
Noma kelvin <165056190+Kelvinbex2@users.noreply.github.com>
2025-05-12 10:46:09 +0200

    update npc

---
+++ proyexto_final/scene/npc/npc.gd
@@ -0,0 +1,3 @@
+@icon("res://Assets/character/npc/icon/npc.svg")
+class_name Npc 
+extends CharacterBody2D
---
+++ proyexto_final/scene/npc/npc_resource.gd
@@ -0,0 +1,6 @@
+class_name NpcResource
+extends Resource
+@export var npc_name : String =""
+@export var sprite : Texture
+@export var potrait : Texture
+#@export var dialogo
