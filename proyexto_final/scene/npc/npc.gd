b76b396efcc0a031477d7a7dcea2b9b1535f2cda
Noma kelvin <165056190+Kelvinbex2@users.noreply.github.com>
2025-05-12 12:29:37 +0200

    update npc

--- proyexto_final/scene/npc/npc.gd
+++ proyexto_final/scene/npc/npc.gd
@@ -1,3 +1,4 @@
+@tool
 @icon("res://Assets/character/npc/icon/npc.svg")
 class_name Npc 
 extends CharacterBody2D
@@ -9,14 +10,22 @@ var state : String = "idle"
 var direction : Vector2 = Vector2.DOWN
 var direction_name : String = "down"
 
-@export var npc_resoure : NpcResource
+@export var npc_resoure : NpcResource : set = _set_npc_resource
 
 
 
 func _ready() -> void:
 	setup_npc()
+	if Engine.is_editor_hint():
+		return
 	
 	
 func setup_npc() -> void:
 	if npc_resoure:
-		animated_sprite.frames = npc_resoure.sprite_frames
+		if animated_sprite:
+			animated_sprite.frames = npc_resoure.sprite_frames
+
+func _set_npc_resource(_npc: NpcResource) -> void:
+	npc_resoure = _npc
+	setup_npc()
+	
