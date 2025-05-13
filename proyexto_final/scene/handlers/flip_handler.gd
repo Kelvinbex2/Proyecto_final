e0873f89b8622851245e5b12c4868b85d86b56b5
Noma kelvin <obamedo20@gmail.com>
2025-05-13 15:43:02 +0200

    update knight flipping

--- proyexto_final/scene/handlers/flip_handler.gd
+++ proyexto_final/scene/handlers/flip_handler.gd
@@ -2,7 +2,7 @@ class_name FlipHandler
 extends Node2D
 
 func handle_flip(body: CharacterBody2D) -> void:
-	var sprite = body.get_node_or_null("AnimatedSprite2D")
+	var sprite := body.get_node_or_null("AnimatedSprite2D")
 	if sprite and sprite is AnimatedSprite2D:
 		if body.velocity.x < 0:
 			sprite.flip_h = true
