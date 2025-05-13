629f5773b54a4223dd0ff67c0c93a585f06c10fc
Noma kelvin <obamedo20@gmail.com>
2025-05-13 15:18:31 +0200

    update

--- proyexto_final/scene/Enemy/knight_base.gd
+++ proyexto_final/scene/Enemy/knight_base.gd
@@ -1,7 +1,5 @@
 extends CharacterBody2D
 
-
-
 #region handlers
 @onready var flip_handler: FlipHandler = $FlipHandler
 @onready var gravity_handler: GravityHandler = $GravityHandler
@@ -12,21 +10,45 @@ extends CharacterBody2D
 @onready var health_handler: HealthHandler = $HealthHandler
 @onready var hurt_box: HurtBox = $HurtBox
 @onready var hit_box_handler: HitBoxHandler = $HitBoxHandler
-
 #endregion
+
 @onready var animatedSprite2D: AnimatedSprite2D = $AnimatedSprite2D
+@onready var detection_area: Area2D = $DetectionArea
+
+var is_attacking: bool = false
+var is_player_in_range: bool = false
 
 func _ready() -> void:
 	hurt_box.area_entered.connect(on_player_hit)
-	
+	detection_area.body_entered.connect(_on_detection_area_entered)
+	detection_area.body_exited.connect(_on_detection_area_exited)
+
 func _physics_process(delta: float) -> void:
-	gravity_handler.apply_gravity(self,delta)
-	ai_handler.handle_state(self,delta)
-	
-	move_and_slide()
-	
-	flip_handler.handle_flip(self)
+	if not is_attacking:
+		gravity_handler.apply_gravity(self, delta)
+		ai_handler.handle_state(self, delta)
+		move_and_slide()
+		flip_handler.handle_flip(self)
 
-
-func on_player_hit(area: Area2D)-> void:
+func on_player_hit(area: Area2D) -> void:
 	drop_handler.add_coin(1)
+
+func _on_detection_area_entered(body: Node) -> void:
+	if body.is_in_group("Player"):
+		is_player_in_range = true
+		if not is_attacking:
+			attack_loop()
+
+func _on_detection_area_exited(body: Node) -> void:
+	if body.is_in_group("Player"):
+		is_player_in_range = false
+
+func attack_loop() -> void:
+	is_attacking = true
+	while is_player_in_range:
+		print("👊 Atacando jugador")
+		animatedSprite2D.play("hit")
+		await animatedSprite2D.animation_finished
+		await get_tree().create_timer(1.0).timeout  # Tiempo entre ataques
+	animatedSprite2D.play("idle")
+	is_attacking = false
--- proyexto_final/scene/handlers/flip_handler.gd
+++ proyexto_final/scene/handlers/flip_handler.gd
@@ -1,11 +1,10 @@
 class_name FlipHandler
 extends Node2D
 
-
-
-
 func handle_flip(body: CharacterBody2D) -> void:
-	if body.velocity.x < 0:
-		body.animatedSprite2D.flip_h = true
-	elif body.velocity.x > 0:
-		body.animatedSprite2D.flip_h = false
+	var sprite = body.get_node_or_null("AnimatedSprite2D")
+	if sprite and sprite is AnimatedSprite2D:
+		if body.velocity.x < 0:
+			sprite.flip_h = true
+		elif body.velocity.x > 0:
+			sprite.flip_h = false
