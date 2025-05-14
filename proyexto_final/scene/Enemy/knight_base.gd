a5a61ddc6f1efd14b53a6cb24c871c54e3304a72
Noma kelvin <165056190+Kelvinbex2@users.noreply.github.com>
2025-05-14 12:44:24 +0200

    update knight

--- proyexto_final/scene/Enemy/knight_base.gd
+++ proyexto_final/scene/Enemy/knight_base.gd
@@ -15,10 +15,9 @@ extends CharacterBody2D
 @onready var animatedSprite2D: AnimatedSprite2D = $AnimatedSprite2D
 @onready var detection_area: Area2D = $DetectionArea
 
-
-
 var is_attacking: bool = false
 var is_player_in_range: bool = false
+var is_dying: bool = false
 
 func _ready() -> void:
 	hurt_box.area_entered.connect(on_player_hit)
@@ -26,11 +25,13 @@ func _ready() -> void:
 	detection_area.body_exited.connect(_on_detection_area_exited)
 
 func _physics_process(delta: float) -> void:
-	if not is_attacking:
-		gravity_handler.apply_gravity(self, delta)
-		ai_handler.handle_state(self, delta)
-		move_and_slide()
-		flip_handler.handle_flip(self)
+	if is_attacking or health_handler.is_dead or is_dying:
+		return
+
+	gravity_handler.apply_gravity(self, delta)
+	ai_handler.handle_state(self, delta)
+	move_and_slide()
+	flip_handler.handle_flip(self)
 
 func on_player_hit(area: Area2D) -> void:
 	drop_handler.add_coin(1)
@@ -39,48 +40,46 @@ func _on_detection_area_entered(body: Node) -> void:
 	if body.is_in_group("Player"):
 		is_player_in_range = true
 
-		# Forzar dirección para flip visual
 		if body.global_position.x < global_position.x:
-			velocity.x = -1  # izquierda
+			velocity.x = -1
 		else:
-			velocity.x = 1   # derecha
+			velocity.x = 1
 
-		# Ejecutar flip visual usando mi sistema actual
 		flip_handler.handle_flip(self)
 
-		if not is_attacking:
+		if not is_attacking and not is_dying:
 			attack_loop()
-
 
 func _on_detection_area_exited(body: Node) -> void:
 	if body.is_in_group("Player"):
 		is_player_in_range = false
 
-
-
 func attack_loop() -> void:
+	if is_attacking or health_handler.is_dead or is_dying:
+		return
+		
 	is_attacking = true
 
-	while is_player_in_range:
+	while is_player_in_range and not health_handler.is_dead and not is_dying:
 		print("👊 Atacando jugador")
-
-		# Activar el hitbox justo antes del golpe
 		hit_box_handler.collision_shape_2d.set_deferred("disabled", false)
-
 		animatedSprite2D.play("hit")
 		await animatedSprite2D.animation_finished
-
-		# Desactivar el hitbox justo después del golpe
 		hit_box_handler.collision_shape_2d.set_deferred("disabled", true)
 
-		if not is_player_in_range:
+		if not is_player_in_range or health_handler.is_dead:
 			break
 
 		await get_tree().create_timer(1.0).timeout
 
-		if not is_player_in_range:
-			break
-
-	# Fin del ataque
-	animatedSprite2D.play("walk")
 	is_attacking = false
+	if not health_handler.is_dead and not is_dying:
+		animatedSprite2D.play("walk")
+
+func play_death_animation() -> void:
+	print("💀 Reproduciendo animación 'die'")
+	is_dying = true
+	is_attacking = false
+	animatedSprite2D.play("die")
+	await animatedSprite2D.animation_finished
+	queue_free()
