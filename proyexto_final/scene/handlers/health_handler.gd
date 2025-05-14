5984950f094b6212feed431dc64f3f2add4bb607
Noma kelvin <165056190+Kelvinbex2@users.noreply.github.com>
2025-05-14 13:00:21 +0200

    update

--- proyexto_final/scene/handlers/health_handler.gd
+++ proyexto_final/scene/handlers/health_handler.gd
@@ -23,11 +23,8 @@ func damage(val: int) -> void:
 		
 	match type:
 		"Player":
-			current_health -= val
-			current_health = max(current_health, 0)
-			GlobalStat.currently_held_coins = current_health
-			SignalBus.emit_on_coin_counter_update(current_health)
 			SignalBus.emit_on_hit(1)
+			current_health -= val
 			
 		"Enemy":
 			current_health -= val
