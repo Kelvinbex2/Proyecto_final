1a3eb211230df53661661c14b7b2bb5ba30361d8
Noma kelvin <165056190+Kelvinbex2@users.noreply.github.com>
2025-05-16 10:29:31 +0200

    update

--- proyexto_final/scene/Diaglog/interact/dialog/dialog_interraction.gd
+++ proyexto_final/scene/Diaglog/interact/dialog/dialog_interraction.gd
@@ -13,11 +13,31 @@ func _ready() -> void:
 	if Engine.is_editor_hint():
 		return
 		
+	
+	area_entered.connect(_on_area_enter)
+	area_exited.connect(_on_area_exit)
+		
 	for i in get_children():
 		if i is DialogItem:
 			dialog_items.append(i)
 
+func player_interact() -> void:
+	player_interacted.emit()
+	
 
+func _on_area_enter(_a :Area2D ) -> void:
+	if enabled == false || dialog_items.size() ==0:
+		return
+		
+	animation.play("show")
+	SignalBus.interact_pressed.connect(player_interact)
+	
+	
+func _on_area_exit(_a :Area2D ) -> void:
+	animation.play("hide")
+	SignalBus.interact_pressed.disconnect(player_interact)
+	
+	
 func _get_configuration_warnings() -> PackedStringArray:
 	if _check_for_dialog_item() == false:
 		return ["Se requiere al menos un nodo DialogItem."]
