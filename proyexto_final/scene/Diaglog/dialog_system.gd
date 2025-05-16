abace6de3a0fe592dd1e84d8b7d044f1a7f6d2ad
Noma kelvin <165056190+Kelvinbex2@users.noreply.github.com>
2025-05-16 08:39:29 +0200

    Update dialog_system.gd

--- proyexto_final/scene/Diaglog/dialog_system.gd
+++ proyexto_final/scene/Diaglog/dialog_system.gd
@@ -1,3 +1,11 @@
-
+@tool
 class_name DialogSysytemNode 
 extends CanvasLayer
+
+
+func _ready() -> void:
+	if Engine.is_editor_hint():
+		if get_viewport() is Window:
+			get_parent().remove_child(self)
+			return
+		return
