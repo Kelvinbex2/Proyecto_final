c9ca6b64abd30d14a5b3009c1a931d8494017b7c
Noma kelvin <obamedo20@gmail.com>
2025-05-17 17:22:16 +0200

    update

--- proyexto_final/scene/Enemy/bat.gd
+++ proyexto_final/scene/Enemy/bat.gd
@@ -1,1 +1,13 @@
 extends CharacterBody2D
+
+#region Handlers
+@onready var flip_handler: FlipHandler = $Handlers/FlipHandler
+@onready var gravity_handler: GravityHandler = $Handlers/GravityHandler
+@onready var movement_handler: MovementHandler = $Handlers/MovementHandler
+@onready var death_handler: DeathHandler = $Handlers/DeathHandler
+@onready var drop_handler: DropHandler = $Handlers/DropHandler
+@onready var health_handler: HealthHandler = $Handlers/HealthHandler
+@onready var hurt_box: HurtBox = $Handlers/HurtBox
+@onready var hit_box_handler: HitBoxHandler = $Handlers/HitBoxHandler
+
+#endregion
