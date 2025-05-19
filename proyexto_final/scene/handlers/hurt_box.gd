ed32266e652d84d01d42c386606797cc6419933c
Noma kelvin <165056190+Kelvinbex2@users.noreply.github.com>
2025-05-19 13:26:25 +0200

    update hurt audio

--- proyexto_final/scene/handlers/hurt_box.gd
+++ proyexto_final/scene/handlers/hurt_box.gd
@@ -2,6 +2,7 @@ class_name HurtBox
 extends Area2D
 
 @onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
+@onready var audio: AudioStreamPlayer2D = $Audio
 
 func _ready() -> void:
 	area_entered.connect(on_area_entered)
@@ -14,9 +15,11 @@ func on_area_entered(area: Area2D) -> void:
 	var health = get_parent().get_node_or_null("HealthHandler")
 	if health:
 		health.damage(1)
-	
-		var blood_scene = load("res://scene/effect/blood_effect.tscn")
-		var blood_instance = blood_scene.instantiate()
-		get_tree().current_scene.add_child(blood_instance)
-		blood_instance.global_position = global_position
-		blood_instance.emitting = true
+	if audio:
+		audio.play()
+		
+	var blood_scene = load("res://scene/effect/blood_effect.tscn")
+	var blood_instance = blood_scene.instantiate()
+	get_tree().current_scene.add_child(blood_instance)
+	blood_instance.global_position = global_position
+	blood_instance.emitting = true
