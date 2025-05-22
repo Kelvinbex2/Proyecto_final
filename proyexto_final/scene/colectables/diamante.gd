11a1283643e3482596434bf150c76c37ac1f78c4
Noma kelvin <obamedo20@gmail.com>
2025-05-22 17:48:53 +0200

    update

--- proyexto_final/scene/colectables/diamante.gd
+++ proyexto_final/scene/colectables/diamante.gd
@@ -1,5 +1,7 @@
 extends Area2D
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+@onready var pickup_sound: AudioStreamPlayer = $PickupSound
+
 
 func _ready():
 	connect("body_entered", _on_body_entered)
@@ -7,4 +9,10 @@ func _ready():
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Player") and body.has_method("add_frutas"):
 		body.add_frutas()
+		
+	
+		if pickup_sound:
+			pickup_sound.play()
+	
+		await get_tree().create_timer(0.1).timeout
 		queue_free()
