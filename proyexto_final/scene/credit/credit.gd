a2a6b80db8b0d575c8647fa98ee676088494eeae
Noma kelvin <165056190+Kelvinbex2@users.noreply.github.com>
2025-05-22 12:32:50 +0200

    Update credit.gd

--- proyexto_final/scene/credit/credit.gd
+++ proyexto_final/scene/credit/credit.gd
@@ -3,18 +3,21 @@ extends CanvasLayer
 @onready var rich_text: RichTextLabel = $Control/PanelContainer/RichTextLabel
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var audio: AudioStreamPlayer = $Control/AudioStreamPlayer
-@onready var tween := create_tween()
 @onready var texture_rect: TextureRect = $TextureRect
+@onready var panel_container := $Control/PanelContainer
 
 var credits_loaded := false
 
 func _ready() -> void:
 	texture_rect.z_index = -1
 	texture_rect.mouse_filter = Control.MOUSE_FILTER_IGNORE
-	
+
 	audio.play()
 	animation_player.play("pop_out")
-	tween.tween_property($Control/PanelContainer, "position:y", -500, 20.0)
+
+	
+	var screen_height = get_viewport().get_visible_rect().size.y
+	panel_container.position.y = screen_height
 
 	var loader = TextEffectsLoader.new()
 	add_child(loader)
@@ -28,6 +31,17 @@ func _ready() -> void:
 		rich_text.clear()
 		rich_text.parse_bbcode(content)
 		credits_loaded = true  
+
 		
+		await get_tree().process_frame
+		await get_tree().process_frame
+
+	
+		print("Texto size Y:", rich_text.size.y)
+
+		var target_y = -panel_container.size.y  
+		var tween = create_tween()
+		tween.tween_property(panel_container, "position:y", target_y, 20.0)
+
 		await get_tree().create_timer(20.0).timeout
 		get_tree().change_scene_to_file("res://scene/ui/main_menu.tscn")
