9cbcc282d4aa489cacf2db15284413f9d363bf2a
Noma kelvin <165056190+Kelvinbex2@users.noreply.github.com>
2025-05-22 12:24:34 +0200

    update

--- proyexto_final/scene/credit/credit.gd
+++ proyexto_final/scene/credit/credit.gd
@@ -3,12 +3,18 @@ extends CanvasLayer
 @onready var rich_text: RichTextLabel = $Control/PanelContainer/RichTextLabel
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var audio: AudioStreamPlayer = $Control/AudioStreamPlayer
+@onready var tween := create_tween()
+@onready var texture_rect: TextureRect = $TextureRect
 
 var credits_loaded := false
 
 func _ready() -> void:
+	texture_rect.z_index = -1
+	texture_rect.mouse_filter = Control.MOUSE_FILTER_IGNORE
+	
 	audio.play()
 	animation_player.play("pop_out")
+	tween.tween_property($Control/PanelContainer, "position:y", -500, 20.0)
 
 	var loader = TextEffectsLoader.new()
 	add_child(loader)
