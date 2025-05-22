d9bad153e544f42876e5873132d4ff22ded62b62
Noma kelvin <165056190+Kelvinbex2@users.noreply.github.com>
2025-05-22 08:13:05 +0200

    update

--- proyexto_final/scene/background/back_ground.gd
+++ proyexto_final/scene/background/back_ground.gd
@@ -3,4 +3,4 @@ extends ParallaxBackground
 @export var scroll_speed: float = 20.0
 
 func _process(delta: float) -> void:
-	scroll_offset.x += scroll_speed * delta
+	##scroll_offset.x += scroll_speed * delta
