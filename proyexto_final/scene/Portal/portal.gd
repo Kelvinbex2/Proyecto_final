2bfdcf08ff5899d284471de5e604d8bae41cc38e
Noma kelvin <165056190+Kelvinbex2@users.noreply.github.com>
2025-05-26 10:46:35 +0200

    update

--- proyexto_final/scene/Portal/portal.gd
+++ proyexto_final/scene/Portal/portal.gd
@@ -1,9 +1,23 @@
 extends Area2D
 
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+
+var activated := false
+
 func _ready():
 	connect("body_entered", _on_body_entered)
+	# Establecer animación cerrada al inicio
+	animated_sprite.play("close")
 
 func _on_body_entered(body: Node) -> void:
+	if activated:
+		return
+
 	if body is Player:
+		activated = true
+		if animated_sprite.sprite_frames.has_animation("open"):
+			animated_sprite.play("open")
+			await animated_sprite.animation_finished  # Espera que termine la animación
+
 		SignalBus.emit_signal("on_portal_triggered")
 		set_deferred("monitoring", false)
